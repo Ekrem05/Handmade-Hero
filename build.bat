be428eeb38c6cba93fb4edd7ffc38cb47426b8a1
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-07 11:54:28 +0300

    configurations

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+mkdir ..\..\build
+pushd ..\...\build
+cl ..\Handmade-Hero\win32_handmade.cpp
+popd
---
+++ win32_handmade.cpp
@@ -0,0 +1,8 @@
+
+#include<Windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PSTR lpCmdLine, int nCmdShow)
+{
+    OutputDebugStringA("Hello\n");
+    return 0;
+}
